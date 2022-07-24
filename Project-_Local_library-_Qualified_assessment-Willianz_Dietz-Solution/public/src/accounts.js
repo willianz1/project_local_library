0844b6414dd4c4d0df520867dc8cf8f7afa02ce2
WillianzD <willianz1999@gmail.com>
2022-07-24 15:12:57 -0700

    Update accounts.js
    added notes to functions

--- Project-_Local_library-_Qualified_assessment-Willianz_Dietz-Solution/public/src/accounts.js
+++ Project-_Local_library-_Qualified_assessment-Willianz_Dietz-Solution/public/src/accounts.js
@@ -1,16 +1,17 @@
 function findAccountById(accounts, id) {
-  const found = accounts.find((account) => account.id === id);
+  const found = accounts.find((account) => account.id === id); //returns the accounts that match the id
   return found;   
 }
 
 function sortAccountsByLastName(accounts) {
-  return accounts.sort((a, b) => a.name.last.toLowerCase() > b.name.last.toLowerCase() ? 1 : -1);  
+  return accounts.sort((a, b) => a.name.last.toLowerCase() > b.name.last.toLowerCase() ? 1 : -1);  //sorts from last name 
 }
 
 function getTotalNumberOfBorrows(account, books) {
   const accountId = account.id; 
   let accu = 0; 
-  books.forEach(book => book.borrows.forEach(borrow => accountId === borrow.id && accu++))  
+  //goes through all of books and checks id matches, then  add accum. 
+  books.forEach(book => book.borrows.forEach(borrow => accountId === borrow.id && accu++))
   return accu; 
   }
 
@@ -19,6 +20,7 @@ function getBooksPossessedByAccount(account, books, authors) {
   let result=[];
   //check for the account id in the borrows arrays
   books.forEach(book => book.borrows.find(borrow => borrow.id === account.id && !borrow.returned && result.push(book)))
+  //for everything in result we create an object and returns author
   result.forEach(book=>{
     const author = authors.find(person => person.id === book.authorId);
     book['author'] = author;
